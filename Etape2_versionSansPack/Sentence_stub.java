66d736457bb8bf9a822463ed0ef8c7e0c192e92e
Mariyanayagam Damien <dmariyan@muse.enseeiht.fr>
2014-01-20 20:10:40 +0100

    Ajout de l'etape2

---
+++ Etape2_versionSansPack/Sentence_stub.java
@@ -0,0 +1,14 @@
+public class Sentence_stub extends SharedObject implements Sentence_itf, java.io.Serializable {
+private static final long serialVersionUID = 1L;
+public void write(java.lang.String param0) {
+lock_write();
+((Sentence) this.obj).write(param0);
+unlock();
+}
+public java.lang.String read() {
+lock_read();
+java.lang.String retour = ((Sentence) this.obj).read();
+unlock();
+return retour;
+}
+}
